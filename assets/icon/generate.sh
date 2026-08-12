3ba00cadaec22f55ce36689a11b7bc6d0ad2c928
Clownacy <Clownacy@users.noreply.github.com>
2026-08-12 21:02:32 +0100

    Add a new icon.
    Now just a hand-drawn 'micro' symbol, instead of a copy of
    ClownMapEd's icon.

--- assets/icon/generate.sh
+++ assets/icon/generate.sh
@@ -2,7 +2,7 @@ magick icon-master.png -resize 128x128 icon-128.png
 magick icon-master.png -resize 64x64 icon-64.png
 magick icon-master.png -resize 48x48 icon-48.png
 magick icon-master.png -resize 40x40 icon-40.png
-#magick icon-master.png -resize 32x32 icon-32.png
+magick icon-master.png -resize 32x32 icon-32.png
 magick icon-master.png -resize 24x24 icon-24.png
 magick icon-master.png -resize 20x20 icon-20.png
 magick icon-master.png -resize 16x16 icon-16.png
