5f2481cc2e74024d4051d9a4de2751caf6107b5c
Clownacy <Clownacy@users.noreply.github.com>
2026-08-12 21:20:13 +0100

    Fix DPI scaling.
    I forgot, Qt5 is dumb.

--- main.cpp
+++ main.cpp
@@ -19,6 +19,14 @@
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+	// Qt6 does this by default.
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+	QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
+#endif
+
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
